3f35ecc238b06857b63c3e820ff58cdb2ddf0973
Romain <98345704+Romlion@users.noreply.github.com>
2023-11-20 08:56:58 +0100

    inclued props

--- PokeMonsters/src/components/PokemonCard.jsx
+++ PokeMonsters/src/components/PokemonCard.jsx
@@ -1,11 +1,13 @@
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc != null ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
