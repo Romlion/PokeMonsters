91bfae5955823d0b10cebc4625a958a883164dce
Romain <98345704+Romlion@users.noreply.github.com>
2023-11-21 08:20:35 +0100

    finished state.1

--- PokeMonsters/src/App.jsx
+++ PokeMonsters/src/App.jsx
@@ -1,9 +1,54 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const pickPrevious = () => setPokemonIndex(pokemonIndex - 1);
+
+  const pickNext = () => setPokemonIndex(pokemonIndex + 1);
+
   return (
     <div>
-      <PokemonCard />
+      <nav>
+        {pokemonIndex > 0 && (
+          <button type="button" onClick={pickPrevious}>
+            previous
+          </button>
+        )}
+        {pokemonIndex < pokemonList.length - 1 && (
+          <button type="button" onClick={pickNext}>
+            next
+          </button>
+        )}
+      </nav>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
   );
 }
